bfa61d118faa4f13b8677c81d74a9091f5b6fcf6
unibox <unibox@duck.com>
2026-01-28 15:35:05 +0100

    add move function for animations

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,28 @@
-export function moveUp() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
+export function move(direction: string = 'up', hide: boolean = true) {
+  const els = document.querySelectorAll(
+    `[data-move]`,
+  ) as NodeListOf<HTMLElement>;
+
+  const windowHeight = document.documentElement.clientHeight;
+  const windowBottomLimit = windowHeight * 0.85;
+
+  function onScroll() {
+    els.forEach((el) => {
+      if (hide) {
+        el.style.opacity = '0';
+      }
+      const elDistanceFromTop = el.getBoundingClientRect().top;
+      switch (direction) {
+        default:
+          if (elDistanceFromTop < windowBottomLimit) {
+            el.style.animation = 'moveUp 1s ease-in-out forwards';
+          } else if (elDistanceFromTop > windowHeight) {
+            el.style.animation = '';
+          }
+          break;
+      }
+    });
+  }
+  onScroll();
+  document.addEventListener('scroll', onScroll);
 }
